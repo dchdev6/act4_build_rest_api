f95d917284aca2d2ee43776aabb40aa5d9975326
Sean Joseph C. Arcana <chdev6@gmail.com>
2025-02-21 12:26:30 +0800

    Update Commit

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express"
 import * as dotevnv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
+import { userRouter } from "./users/users.routes"
+import { productRouter } from "./products/product.routes"
 
 dotevnv.config()
 
@@ -10,6 +12,7 @@ if (!process.env.PORT) {
 }
 
 const PORT = parseInt(process.env.PORT as string, 10)
+
 const app = express()
 
 app.use(express.json())
@@ -17,6 +20,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(helmet())
 
+app.use ('/', userRouter)
+app.use ('/', productRouter)
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
